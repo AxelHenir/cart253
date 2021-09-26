82002de701bf5dc570ed691ee5561fbacf628e1a
AxelHenir <axelhenirmail@gmail.com>
2021-09-25 23:29:36 -0400

    Added difficulty scaling, WASD move
    Start working on the covid spawning system next. You need a constructor.

--- Exercises/dodge-em/js/script.js
+++ Exercises/dodge-em/js/script.js
@@ -1,20 +1,35 @@
 // Alex Henri
 /*
-Exercise 4: Dodge-Em
+Exercise 4: Dodge-Em ===========================================================
 
 My program will utilize the activity 4 code: COVID19 as its basis.
-I have made the following additions:
-I have improved onthe following functions:
+
+Here is the  list of applied changes:
+-Made difficulty parameter to control difficulty and scaling for COVID19
+-position is now controlled by WASD.
+
 
 to do:
--
+
+-left click = dash for immunity on cooldown
+-hold right click = shield + slowed movement
+-show boost and shield cooldowns
+-Covid19 can spawn at an angle
+-Covid19 can respawn with new size, speed, angle
+-display score upon death
+-explain that you died upon death
+-covid19 has a sprite (virus)
+-user has a sprite (medic)
+
 */
 
 "use strict";
 
 let covid19 = {x:0,y:250,size:100,vx:0,vy:0,speed:5,fill:{r:255,g:0,b:0}};
-let user = {size:100,fill:255};
-let staticAmount = 1000;
+let staticAmount = 0;
+let difficulty = {cSpeed:1, cSize:1, growthRate:1};
+let user = {x:0,y:0,size:100,fill:255,vx:5,vy:5};
+let covidHold = new Array();
 
 function preload(){}
 
@@ -22,7 +37,6 @@ function setup() {
   createCanvas(windowWidth,windowHeight);
   covid19.y = random(0,windowHeight);
   covid19.vx = covid19.speed;
-
 }
 
 function draw() {
@@ -35,28 +49,43 @@ function draw() {
     stroke(255);
     point(x,y);
   }
-  noStroke();
+  noStroke(); // Printing Covid19 + User =======================================
 
-  covid19.x += covid19.vx;
-  covid19.y += covid19.vy;
-  fill(covid19.fill.r,covid19.fill.g,covid19.fill.b);
+covidHold.push(new covid19());
+for (var i=0; i<covidHold.length; i++){ //Move Covid(s)
+  covidHold[i].x =+covidHold[i].vx;
+  covidHold[i].y =+covidHold[i].vy;
+}
 
-  ellipse(covid19.x,covid19.y,covid19.size); // Draw Covid19
+  if(keyIsDown(87)){ // W - Up
+    user.y-=user.vy;
+  }
+  if(keyIsDown(65)){ // A - Left
+    user.x-=user.vy;
+  }
+  if(keyIsDown(83)){ // S - Down
+    user.y+=user.vy;
+  }
+  if(keyIsDown(68)){ // D - Right
+    user.x+=user.vy;
+  }
 
-  user.x=mouseX;
-  user.y=mouseY;
+  constrain(user.x,0,windowWidth); // Constrain user to window.
+  constrain(user.y,0,windowHeight);
+
   fill(user.fill);
-
   ellipse(user.x,user.y,user.size); // Draw user
 
+  fill(covid19.fill.r,covid19.fill.g,covid19.fill.b);
+  ellipse(covid19.x,covid19.y,covid19.size); // Draw Covid19
+
   let d = dist(covid19.x,covid19.y,user.x,user.y);
-  if (d < (covid19.size/2 + user.size/2)){
+  if (d < (covid19.size/2 + user.size/2)){ // Death.
+
     noLoop();
   }
 
-
-
-  if (covid19.x > windowWidth){
+  if (covid19.x > windowWidth){ // Respawning mechanism
     covid19.x = 0;
     covid19.y = random(0,windowHeight);
 
