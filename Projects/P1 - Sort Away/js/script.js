de2c2cd4db65a1d53b9a85bbd9d8c8b562d2774a
AxelHenir <axelhenirmail@gmail.com>
2021-10-06 23:16:40 -0400

    Added sorting stuff

--- Projects/P1 - Sort Away/js/script.js
+++ Projects/P1 - Sort Away/js/script.js
@@ -3,19 +3,18 @@
 "use strict";
 
 let CW = 1000; // Canvas Width (CW)
-let CANDLE_AMOUNT=100; // Number of candles to be sorted
+let CANDLE_AMOUNT=75; // Number of candles to be sorted
 
 let state = 'title'; // Can be: (title, running, paused, finished)
 
-let candle ={
-  width:CW/CANDLE_AMOUNT,
-  height:0,
-  y:CW,
-  x:0,
-  
+let cHolder = new Array();
+function candle(){
+  this.width=CW/CANDLE_AMOUNT;
+  this.height=0;
+  this.y=CW;
+  this.x=0;
+  this.hue=0;
 }
-
-
 
 function preload() {
 
@@ -23,9 +22,17 @@ function preload() {
 
 function setup() {
   createCanvas(CW,CW);
+
+  setupCandles();
+  scrambleCandles();
+
+  bubbleSort();
+  showCandles();
+
 }
 
 function draw() {
+
   switch(state){
     case 'title':
       title();
@@ -33,6 +40,8 @@ function draw() {
     case 'running':
       running();
       break;
+    case 'finished':
+      finished();
   }
 }
 
@@ -42,4 +51,52 @@ function title(){
 
 function running(){
 
+}
+
+function finished(){
+  console.log('Done!');
+}
+
+function setupCandles(){
+  for(let i=0;i<CANDLE_AMOUNT;i++){
+    for (let i=0;i<CANDLE_AMOUNT;i++){
+      cHolder.push(new candle);
+    }
+    cHolder[i].x = cHolder[i].width*i;
+    cHolder[i].height = (cHolder[i].width*i)+cHolder[i].width;
+    cHolder[i].hue = cHolder[i].height*360/CW // Hue depends on height
+  }
+}
+
+function scrambleCandles(){
+  for(let i = 0; i<150; i++){
+    let a = int(random(0,CANDLE_AMOUNT));
+    let b = int(random(0,CANDLE_AMOUNT));
+    let temp = cHolder[a].x;
+    cHolder[a].x=cHolder[b].x;
+    cHolder[b].x=temp;
+  }
+
+}
+
+function showCandles(){
+  for(let i=0;i<CANDLE_AMOUNT;i++){
+    colorMode(HSL);
+    rectMode(CORNERS);
+    fill(cHolder[i].hue,100,50);
+    rect(cHolder[i].x,cHolder[i].y,cHolder[i].x+cHolder[i].width,height-cHolder[i].height);
+  }
+}
+
+function bubbleSort(){
+  let sorted=false;
+   for (let i = 0; i < CANDLE_AMOUNT - 1; i++){
+     for(let j= 0; j < CANDLE_AMOUNT - i - 1; j++){
+       if(cHolder[j].x > cHolder[j+1].x){
+         let temp = cHolder[j].x;
+         cHolder[j].x=cHolder[j+1].x;
+         cHolder[j+1].x=temp;
+       }
+     }
+   }
 }
