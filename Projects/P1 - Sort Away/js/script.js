089d546614c9d4606689ed5e4f1fa646670100ac
AxelHenir <axelhenirmail@gmail.com>
2021-10-12 13:47:49 -0400

    what was I thinking LOL

--- Projects/P1 - Sort Away/js/script.js
+++ Projects/P1 - Sort Away/js/script.js
@@ -7,13 +7,9 @@ let CANDLE_AMOUNT=75; // Number of candles to be sorted
 
 let state = 'title'; // Can be: (title, running, paused, finished)
 
-let cHolder = new Array();
-function candle(){
-  this.width=CW/CANDLE_AMOUNT;
-  this.height=0;
-  this.y=CW;
-  this.x=0;
-  this.hue=0;
+let arr = new Array; // Arr will hold the "height" of each candle. Their position within the array will describe their x position.
+for (let i=0;i<CANDLE_AMOUNT;i++){ // Populate array with increasingly taller candles.
+  arr[i]=i+1;
 }
 
 function preload() {
@@ -22,12 +18,10 @@ function preload() {
 
 function setup() {
   createCanvas(CW,CW);
+  background(255);
+  scrambleArr();
+  drawArr();
 
-  setupCandles();
-  scrambleCandles();
-
-  bubbleSort();
-  showCandles();
 
 }
 
@@ -57,45 +51,38 @@ function finished(){
   console.log('Done!');
 }
 
-function setupCandles(){
-  for(let i=0;i<CANDLE_AMOUNT;i++){
-    for (let i=0;i<CANDLE_AMOUNT;i++){
-      cHolder.push(new candle);
-    }
-    cHolder[i].x = cHolder[i].width*i;
-    cHolder[i].height = (cHolder[i].width*i)+cHolder[i].width;
-    cHolder[i].hue = cHolder[i].height*360/CW // Hue depends on height
-  }
-}
-
-function scrambleCandles(){
+function scrambleArr(){ // Call to scramble the candles
   for(let i = 0; i<150; i++){
     let a = int(random(0,CANDLE_AMOUNT));
     let b = int(random(0,CANDLE_AMOUNT));
-    let temp = cHolder[a].x;
-    cHolder[a].x=cHolder[b].x;
-    cHolder[b].x=temp;
+    let temp = arr[a];
+    arr[a]=arr[b];
+    arr[b]=temp;
   }
 
 }
 
-function showCandles(){
-  for(let i=0;i<CANDLE_AMOUNT;i++){
-    colorMode(HSL);
-    rectMode(CORNERS);
-    fill(cHolder[i].hue,100,50);
-    rect(cHolder[i].x,cHolder[i].y,cHolder[i].x+cHolder[i].width,height-cHolder[i].height);
+function drawSwap(a,b){ // Call to draw the swap between arr[a] and arr[b]
+  
+}
+
+function drawArr(){
+  rectMode(CORNERS);
+  fill(0);
+  for (let i=0;i<arr.length;i++){
+    fill(arr[i]*5,0,150);
+    rect(i*CW/CANDLE_AMOUNT,CW,(i*CW/CANDLE_AMOUNT)+CW/CANDLE_AMOUNT,CW-(arr[i]*CW/CANDLE_AMOUNT));
   }
 }
 
-function bubbleSort(){
-  let sorted=false;
+function bubbleSort(){ // [1] - Bubble sort
    for (let i = 0; i < CANDLE_AMOUNT - 1; i++){
      for(let j= 0; j < CANDLE_AMOUNT - i - 1; j++){
-       if(cHolder[j].x > cHolder[j+1].x){
-         let temp = cHolder[j].x;
-         cHolder[j].x=cHolder[j+1].x;
-         cHolder[j+1].x=temp;
+       if(arr[j] > arr[j+1]){
+         drawSwap(j,j+1);
+         let temp = arr[j];
+         arr[j]=arr[j+1];
+         arr[j+1]=temp;
        }
      }
    }
