2922066fefe9393dd5b887a59c59e595dccf8c9b
AxelHenir <axelhenirmail@gmail.com>
2021-11-18 17:06:19 -0500

    demo 1

--- Projects/P2 - Audio/js/script.js
+++ Projects/P2 - Audio/js/script.js
@@ -4,9 +4,16 @@
 "use strict";
 
 let song1,song2,song3,song4;
-let currentSongName="N/A  ";
+let currentSongName="N/A";
 let amp,ftt;
 let music=undefined;
+
+let ampLevel;
+
+let ampCircle ={
+  x:0,
+  y:0,
+}
 
 function preload() {
   song1=loadSound("assets/sounds/music/Cowboy Bebop - Rush.mp3");
@@ -24,6 +31,8 @@ function setup(){
   // FTT object from p5.sound
   ftt = new p5.FFT();
 
+  rectMode(CENTER,CENTER);
+  textAlign(CENTER,CENTER);
 }
 
 
@@ -31,7 +40,71 @@ function draw() {
   background(255);
   noFill();
 
+  // Display info about the current song
   displayMusicInfo();
+
+  // Updates the FFT visualizer
+  handleFFT();
+
+  // Amplitude Demo #1
+  ampDemo1();
+
+  // Draws the borders around the demo windows
+  drawBorders();
+
+}
+
+function ampDemo1(){ // Amp demo 1 shows how the volume affects a circle's size.
+
+  // Check if we are actually playing music first,
+  if(music!=undefined){
+    push();
+    translate(200,200);
+    strokeWeight(3);
+    stroke(0);
+    fill(255);
+    let level = amp.getLevel();
+    let size = map(level,0,1,30,150);
+    ellipse(ampCircle.x,ampCircle.y,size,size);
+    pop();
+  }
+
+}
+
+function handleFFT(){
+
+  // Check if we are actually playing music first,
+  if(music!=undefined){
+
+  }
+
+}
+
+function drawBorders(){
+
+  // Window border
+  push();
+  strokeWeight(5);
+  stroke(0);
+  noFill();
+  rect(500,500,1000,1000);
+  pop();
+
+  // Demo 1
+  push();
+
+  translate(200,200);
+  noFill();
+  strokeWeight(5);
+  stroke(0);
+  rect(0,0,200,200);
+  noStroke();
+  textSize(14);
+  fill(0);
+  text("Amplitude: Circle radius = Overall Volume", 0,120);
+  pop();
+
+  // Demo 2
 }
 
 function keyPressed(){ // Handles all keyboard input. 1-4 to play songs. Space to play/pause.
@@ -95,10 +168,7 @@ function keyPressed(){ // Handles all keyboard input. 1-4 to play songs. Space t
 
 
     case 32: // Spacebar = Play/Pause
-      if(music==undefined){
-
-      }
-      else{
+      if(music!=undefined){
         if(music.isPlaying()){
           music.pause(); // Pause music
         }
@@ -115,6 +185,7 @@ function keyPressed(){ // Handles all keyboard input. 1-4 to play songs. Space t
 function displayMusicInfo(){ // Display the "now-playing"
 
   push();
+  textAlign(LEFT);
   textSize(24);
   fill(0);
   noStroke();
